84796768c7d0df4661d6fafd9e40a444e62c6061
temiltas <temiltas.islakkoz@gmail.com>
2022-01-23 15:30:20 +0300

    Add byte utils

---
+++ test/utils_test.dart
@@ -0,0 +1,49 @@
+import 'dart:typed_data';
+
+import 'package:casper_dart_sdk/src/helpers/byte_utils.dart';
+import 'package:casper_dart_sdk/src/helpers/string_utils.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  test('Can convert byte array to nibbles', () {
+    Uint8List bytes = Uint8List.fromList([0x01, 0xff, 0x35]);
+
+    List<int> nibbles = bytesToNibbles(bytes.toList()).toList();
+
+    expect(nibbles, equals([0x0, 0x1, 0xf, 0xf, 0x3, 0x5]));
+  });
+
+  test('Can convert byte array to bits', () {
+    Uint8List bytes = Uint8List.fromList([0x01, 0xff, 0x35]);
+    List<int> bits = bytesToBits(bytes.toList()).toList();
+    expect(
+        bits,
+        equals([
+          0x0,
+          0x0,
+          0x0,
+          0x0,
+          0x0,
+          0x0,
+          0x0,
+          0x1,
+          0x1,
+          0x1,
+          0x1,
+          0x1,
+          0x1,
+          0x1,
+          0x1,
+          0x1,
+          0x0,
+          0x0,
+          0x1,
+          0x1,
+          0x0,
+          0x1,
+          0x0,
+          0x1
+        ]));
+  });
+}
